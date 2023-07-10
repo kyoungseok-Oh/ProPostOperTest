bf18be2b124e1dc5dd0f6c74561c556510eebef8
kyoungseok-Oh <134459438+kyoungseok-Oh@users.noreply.github.com>
2023-07-10 16:28:39 +0900

    Add files via upload

---
+++ ProPostOperTest.java
@@ -0,0 +1,10 @@
+public class ProPostOperTest {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(--x);
+        System.out.println(x++);
+        System.out.println(x--);
+        System.out.println(++x);
+        System.out.println(x);
+    }
+}
